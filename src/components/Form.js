873f37ec933c0d3829f0442bfab1a4340c7c6773
Ryan Soto <rsoto003@ucr.edu>
2019-06-01 09:04:20 -0700

    form can now successfully log to console user input data

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Form = () => (
-    <div className="input-group mb-3 col-xl-4 mt-3">
-        <input type="text" className="form-control" placeholder="Find a recipe..." aria-label="Recipient's username" aria-describedby="button-addon2" />
-        <div className="input-group-append">
-            <button className="btn btn-secondary" type="button" id="button-addon2">Search</button>
-        </div>
-    </div>
-    );
+class Form extends Component {
+    render(props){
+        return (
 
+            <form className="input-group mb-3 col-xl-4 mt-3" onSubmit={this.props.getRecipe}>
+                <input className="form-control" placeholder="Find a recipe..." type="text" name="recipe"/>
+                <div className="input-group-append">
+                    <button className="btn btn-secondary"  type="submit">Search</button>
+                </div>
+            </form>
+        );
+    }
+}
 export default Form;
