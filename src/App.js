969896169f0f6d2dd49c75ed41ec9dc2c7d8bbd1
Ryan Soto <rsoto003@ucr.edu>
2019-06-01 22:36:31 -0700

    added api key and test call functionality

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import Navbar from './components/Navbar';
 import Form from './components/Form';
 
 class App extends Component {
-  getRecipe = (e) => {
+  
+
+
+  getRecipe = async (e) => {
     e.preventDefault();
+    e.persist();
+    const api_key = '0b526b6bb7c7894d4d0a76610cfb9129';
+    const recipe_call = await fetch(`https://www.food2fork.com/api/search?key=${api_key}&q=shredded%20chicken`);
+    const recipe_data = await recipe_call.json();
+    console.log(recipe_data)
     const recipeName = e.target.elements.recipe.value;
-    e.target.elements.recipe.value = '';
+    e.target.elements.recipe.value = 'Search for another recipe...';
     console.log(recipeName);
   }
   render(){
